068e47f0a2d089edf725503cf0c2697eaaa7070c
Rizky Ario <rizkyario@yahoo.com>
2018-08-04 17:37:47 +0200

    Use redux-saga-tester for integration test

--- src/pages/Messages/tests/sagas.test.js
+++ src/pages/Messages/tests/sagas.test.js
@@ -1,57 +1,59 @@
+/*  eslint-disable no-magic-numbers */
+
 import { onSelectThreadWorker, onLoadMessagesWorker, onLoadThreadsWorker } from 'app/pages/Messages/sagas';
-import { expectSaga } from 'redux-saga-test-plan';
 import messagesStore from 'app/pages/Messages/reducer';
+import authStore from 'app/pages/Auth/reducer';
 import deepFreeze from 'deep-freeze';
-import { select, call } from 'redux-saga/effects';
-import { selectThread as selectThreadSelector } from 'app/pages/Messages/selector';
-import { onLoadPhotosUser } from 'app/pages/Feed/actions';
 import { loadMessages, loadThreads } from 'app/pages/Messages/actions';
-import { selectCurrentUser } from 'app/pages/Auth/selector';
 import * as messageServices from 'app/services/messages.js';
 import * as threadServices from 'app/services/threads.js';
+import SagaTester from 'redux-saga-tester';
 
 describe('messagesSaga', () => {
   const initialState = {
-    messages: [],
-    selectedThread: '',
-    threads: [],
+    messages: {
+      messages: [],
+      selectedThread: '',
+      threads: [],
+    },
   };
   it('onLoadMessagesWorker', () => {
     const threadId = '1';
     const messages = [{ id: '1' }];
-    return expectSaga(onLoadMessagesWorker, { thread: threadId })
-      .withReducer(messagesStore, deepFreeze(initialState))
-      .provide([
-        [call(messageServices.get, threadId), messages],
-      ])
-      .put(loadMessages(messages))
-      .hasFinalState({ ...initialState, messages })
-      .run();
+    const sagaTester = new SagaTester({
+      initialState: deepFreeze({ messages: initialState.messages }),
+      reducers: { messages: messagesStore },
+    });
+    messageServices.get = jest.fn(() => messages);
+    sagaTester.start(onLoadMessagesWorker, { thread: threadId });
+    expect(sagaTester.numCalled(loadMessages().type)).toEqual(2);
+    expect(sagaTester.getState()).toEqual({ messages: { ...initialState.messages, messages } });
   });
   it('onSelectThreadWorker', () => {
     const threadId = '1';
-    const userId = '1';
     const threads = [{ id: '1', user2: { id: '1' } }];
-    return expectSaga(onSelectThreadWorker, { thread: threadId })
-      .withReducer(messagesStore, deepFreeze({ ...initialState, threads }))
-      .provide([
-        [select(selectThreadSelector), { user2: { id: userId } }],
-      ])
-      .put(onLoadPhotosUser(userId))
-      .hasFinalState({ ...initialState, selectedThread: threadId, threads })
-      .run();
+    const sagaTester = new SagaTester({
+      initialState: deepFreeze({ messages: { ...initialState.messages, threads } }),
+      reducers: { messages: messagesStore },
+    });
+    sagaTester.start(onSelectThreadWorker, { thread: threadId });
+    expect(sagaTester.getState())
+      .toEqual({ messages: { ...initialState.messages, selectedThread: threadId, threads } });
   });
   it('onLoadThreadsWorker', () => {
     const userId = '1';
     const threads = [{ id: '1', user2: { id: '1' } }];
-    return expectSaga(onLoadThreadsWorker)
-      .withReducer(messagesStore, deepFreeze(initialState))
-      .provide([
-        [select(selectCurrentUser), { id: userId }],
-        [call(threadServices.get, userId), threads],
-      ])
-      .put(loadThreads(threads))
-      .hasFinalState({ ...initialState, threads })
-      .run();
+    const sagaTester = new SagaTester({
+      initialState: deepFreeze({ ...initialState, auth: { user: { id: userId } } }),
+      reducers: {
+        auth: authStore,
+        messages: messagesStore,
+      },
+    });
+    threadServices.get = jest.fn(() => threads);
+    sagaTester.start(onLoadThreadsWorker);
+    expect(sagaTester.numCalled(loadThreads().type)).toEqual(2);
+    expect(sagaTester.getState())
+      .toEqual({ auth: { user: { id: userId } }, messages: { ...initialState.messages, threads } });
   });
 });
