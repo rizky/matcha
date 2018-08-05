5167ff5400d3e61188ccbb59a90a7bedd8274116
Rizky Ario <rizkyario@yahoo.com>
2018-08-05 16:04:14 +0200

    Refactor api

--- api/routes/models/Message.js
+++ api/routes/models/Message.js
@@ -1,31 +1,24 @@
-import ORM from './ORM'
-import User from './User'
+/*  eslint-disable no-restricted-imports */
+
+import ORM from './ORM';
+import User from './User';
 import Thread from './Thread';
 
 export default class Message extends ORM {
-	static async populate(message)
-	{
-		if (message.from == 0)
-			message.from = { id: 0, name: 'Matcha', username: 'Matcha'}
-		else
-			message.from = await User.findOne(message.from)
-		message.thread = await Thread.findOne(message.thread)
-		message.thread.user1 = await User.findOne(message.thread.user1)
-		message.thread.user2 = await User.findOne(message.thread.user2)
-	}
+  static async populate(message) {
+    const result = message;
+    result.from = await User.findOne(message.from);
+    result.thread = await Thread.findOne(message.thread);
+    result.thread.user1 = await User.findOne(message.thread.user1);
+    result.thread.user2 = await User.findOne(message.thread.user2);
+    return result;
+  }
 
-	static find(params, order, callback)
-	{
-		Message.findAll(params, order, (err, messages) =>
-		{	
-			if (err)
-				callback(err, messages)
-			var promises = messages.map(async (message) => {
-				return await this.populate(message)
-			})
-			Promise.all(promises).then(results => {
-				callback(err, messages)
-			})
-		})
-	}
+  static find(params, order, callback) {
+    Message.findAll(params, order, (err, messages) => {
+      if (err) { callback(err, messages); }
+      const promises = messages.map(async (message) => this.populate(message));
+      Promise.all(promises).then((results) => callback(err, results));
+    });
+  }
 }
